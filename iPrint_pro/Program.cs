fcdcd45d540d7e5847e270b2e67faeaa81ffa675
AndreiYakovenko <andrei.yakovenko@gmail.com>
2024-12-10 15:29:03 +0300

    Enter array price

--- iPrint_pro/Program.cs
+++ iPrint_pro/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Формируем массив, который будет соответствовать нашему прайсу
+        double[,] price = {
+                        {7, 6.65, 6.32, 6},
+                        {10.5, 8.65, 8.21, 7.8},
+                        {5, 4.75, 4.51, 4.29},
+                        {7.5, 6.18, 5.87, 5.57},
+                        {5, 4.75, 4.51, 4.29},
+                        {7.5, 6.18, 5.87, 5.57},
+                        {4, 3.8, 3.61, 3.43},
+                        {6, 4.94, 4.69, 4.46},
+                        {100, 120, 140, 160}
+};
+    }
+}
